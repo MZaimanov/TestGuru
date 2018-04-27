66aed5b41c1c9dc7722c5eb82c984e7783ee74e0
MZaimanov(mike) <mizaimanov@gmail.com>
2018-04-27 12:55:26 +0300

    Lesson 4 Step 3

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,9 +1,8 @@
 class Test < ApplicationRecord
+  has_many :users_tests
+  belongs_to :category
   def self.category_tests(category)
-    Test.joins('
-      JOIN categories
-      ON category_id = categories.id')
-      .where('categories.title = :category', category: category)
-      .order(title: :desc).pluck(:title)
+      category = Category.find_by(title: category)
+      where(category_id: category.id).order(title: :desc).pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,6 @@
 class User < ApplicationRecord
+  has_many :users_tests
   def user_tests(level)
-    Test.joins('
-      JOIN users_tests
-      ON tests.id = users_tests.test_id')
-      .where('users_tests.user_id = :user_id AND level = :level',
-      user_id: self.id, level: level)
+    Test.joins(:users_tests).where(users_tests: {user_id: self.id}, level: level)
   end
 end
