e99284081cabcb5e9bb576ca039906b43a7f3838
MZaimanov(mike) <mizaimanov@gmail.com>
2018-05-17 09:21:35 +0300

    Lesson 6 Step 2

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,15 @@
 class Answer < ApplicationRecord
   belongs_to :question
 
+  scope :correct, -> { where(correct: true) }
+
   validates :body, presence: true
 
-  scope :correct, -> { where(correct: true) }
+  validate :validate_answers_count, on: :update
+
+  private
+
+  def validate_answers_count
+    errors.add(:answers, 'Должно быть от 1 до 4 ответов!') unless (1..4).include?(answers.size)
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -3,11 +3,4 @@ class Question < ApplicationRecord
   belongs_to :test
 
   validates :body, presence: true
-  validate :validate_answers_count, on: :update
-
-  private
-
-  def validate_answers_count
-    errors.add(:answers, 'Должно быть от 1 до 4 ответов!') unless (1..4).include?(answers.size)
-  end
 end
