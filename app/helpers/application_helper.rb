3450106510e2f7f1ee417402473a4b39db483fec
MZaimanov(mike) <mizaimanov@gmail.com>
2018-05-29 14:23:37 +0300

    Lesson 8 Step 1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def current_year
+    Time.current.year
+  end
+
+  def github_url
+    content_tag(:a, 'Код на GitHub', href: 'https://github.com/MZaimanov/TestGuru', target: 'blank')
+  end
 end
