0708225331c6ed792d16d62755ab8d853a2822bd
mike <mizaimanov@gmail.com>
2018-06-08 00:07:41 +0300

    Lesson 12 Step 1

--- Gemfile
+++ Gemfile
@@ -39,6 +39,10 @@ gem 'bootstrap-sass'
 #AUTH
 gem 'devise', '~> 4.0'
 
+# LOCALISATION
+
+gem 'rails-i18n', '~> 5.0.0'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
+
+  def default_url_options
+    { lang: (I18n.locale unless I18n.locale == I18n.default_locale) }
+  end
 
   protected
 
@@ -18,5 +23,9 @@ class ApplicationController < ActionController::Base
     else
       tests_path
     end
+  end
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 end
