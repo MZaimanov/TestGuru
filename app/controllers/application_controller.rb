2b1e0e9c321c375a0277e1edf1e6c541e8554980
MZaimanov(mike) <mizaimanov@gmail.com>
2018-06-09 09:33:34 +0300

    Lesson 12 Step 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
 
   def default_url_options
-    { lang: (I18n.locale unless I18n.locale == I18n.default_locale) }
+    I18n.locale == I18n.default_locale ? {} : { lang: I18n.locale }
   end
 
   protected
