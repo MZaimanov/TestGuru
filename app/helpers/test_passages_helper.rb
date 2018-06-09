70b52ce629c51714dd97c76bfc2ba308602fb73f
MZaimanov(mike) <mizaimanov@gmail.com>
2018-06-09 09:33:34 +0300

    Lesson 12 Step 2

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -4,7 +4,7 @@ module TestPassagesHelper
     test_passage.success? ? "SUCCESS" : "FAIL"
   end
 
-  def result_class(test_passage)
+  def result_string(test_passage)
     test_passage.success? ? t('test_passage.pass') : t('test_passage.fail')
   end
 end
