808d07afe630589c11b6e82e5223b8bdcafdfc2a
Mike <mizaimanov@gmail.com>
2018-07-01 14:49:10 +0300

    Lesson17 Step 7

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,35 @@
-
 categories = Category.create!([
   { title: 'Frontend' },
   { title: 'Backend'},
-  { title: 'Other' }
-  ])
+  { title: 'Frameworks' }])
+
 
 tests = Test.create!([
-  { title: 'HTML',   level: 1, category_id: categories[0].id, author_id: User.first.id },
-  { title: 'Rails',  level: 1, category_id: categories[0].id, author_id: User.first.id },
-  { title: 'Ruby',   level: 2, category_id: categories[1].id, author_id: User.first.id },
-  { title: 'Go',     level: 3, category_id: categories[1].id, author_id: User.first.id },
-  { title: 'SQL',    level: 1, category_id: categories[2].id, author_id: User.first.id }
-  ])
+  { title: 'HTML', level: 0, category_id: categories[0].id, author_id: User.first.id },
+  { title: 'CSS', level: 0, category_id: categories[0].id, author_id: User.first.id },
+  { title: 'Ruby', level: 2, category_id: categories[1].id, author_id: User.first.id },
+  { title: 'Swift', level: 1, category_id: categories[1].id, author_id: User.first.id },
+  { title: 'Rails', level: 3, category_id: categories[2].id, author_id: User.first.id },
+  { title: 'Node', level: 2, category_id: categories[2].id, author_id: User.first.id }])
 
-questions = Question.create!([
-  { body: 'Что такое Ruby?',                          test_id: tests[2].id },
-  { body: 'Назовите тип языка Go',                    test_id: tests[3].id },
-  { body: 'Что обозначает тег head?',                 test_id: tests[0].id },
-  { body: 'Что такое ActiveRecord?',                  test_id: tests[2].id },
-  { body: 'Как расшифровывается аббревиатура SQL?',   test_id: tests[4].id }
-  ])
+questions = Question.create([
+  { body: 'Is HTML for web?', test_id: tests[0].id },
+  { body: 'What version of CSS is actual?', test_id: tests[1].id },
+  { body: 'What is Ruby?', test_id: tests[2].id },
+  { body: 'Swift is game by Nintendo, isn`t it?', test_id: tests[3].id },
+  { body: 'What is Rails?', test_id: tests[4].id },
+  { body: 'What language support Node?', test_id: tests[5].id }])
 
 Answer.create!([
-  { body: 'это камень',
-    correct: false, question_id: questions[0].id },
-  { body: 'компилируемый',
-    correct: true,  question_id: questions[1].id },
-  { body: 'это блочный элемент',
-    correct: true,  question_id: questions[2].id },
-  { body: 'Active Record это M в MVC - модель - которая является слоем в системе, ответственным за представление бизнес-логики и данных.',
-    correct: false, question_id: questions[3].id },
-  { body: 'язык структурированных запросов',
-    correct: false, question_id: questions[4].id }
-  ])
-
-
+  { body: 'No', question_id: questions[0].id },
+  { body: 'Yes', correct: true, question_id: questions[0].id },
+  { body: '2', question_id: questions[1].id },
+  { body: '3', correct: true, question_id: questions[1].id },
+  { body: 'Mineral', question_id: questions[2].id },
+  { body: 'Programming language', correct: true, question_id: questions[2].id },
+  { body: 'Yes', question_id: questions[3].id },
+  { body: 'No', correct: true, question_id: questions[3].id },
+  { body: 'Road for trains', question_id: questions[4].id },
+  { body: 'Ruby`s framework', correct: true, question_id: questions[4].id },
+  { body: 'English', question_id: questions[5].id },
+  { body: 'JavaScript', correct: true, question_id: questions[5].id }])
